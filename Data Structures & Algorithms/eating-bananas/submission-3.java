f671127e22a352008323356b8f4b34c09e9f96c7
Oleksii <demitesh@gmail.com>
2026-04-16 19:23:34 +0200

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int upperB = 0;
+        int l = 1;
+        int timeAtMid = 0;
+        int minK = Integer.MAX_VALUE;
+        int mid = 0;
+
+        for (int pile : piles) {
+            upperB = Math.max(pile, upperB);
+        }
+
+        minK = upperB;
+
+        while (l <= upperB) {
+            mid = (upperB - l) / 2 + l;
+            timeAtMid = 0;
+
+            for (int pile : piles) {
+                timeAtMid += (int) Math.ceil((double) pile / mid);
+            }
+
+            if (timeAtMid > h) {
+                l = mid + 1;
+            } else {
+                upperB = mid - 1;
+                minK = Math.min(minK, mid);
+            }
+        }
+
+        return minK;
+    }
+}
